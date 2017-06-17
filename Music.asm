7cba6537c6baec627afe0093f8d5b0df5482312e
Devwarlt <devwarlt@loesoft.org>
2017-06-17 10:31:40 -0300

    Update Music.asm

--- Music.asm
+++ Music.asm
@@ -4,18 +4,18 @@
 #DEFINE LED_2	PORTB, 2
 #DEFINE SOM	PORTB, 1	; Define headphones
 
-;	DO		-	262Hz
-;	DO#		-	277Hz
-;	RE		-	294Hz
-;	RE#		-	311Hz
-;	MI		-	330Hz
-;	FA		-	349Hz
-;	FA#		-	370Hz
-;	SOL		-	392Hz
-;	SOL#		-	415Hz
-;	LA		-	440Hz
-;	LA#		-	466Hz
-;	SI		-	494Hz
+;	DO	-	262Hz
+;	DO#	-	277Hz
+;	RE	-	294Hz
+;	RE#	-	311Hz
+;	MI	-	330Hz
+;	FA	-	349Hz
+;	FA#	-	370Hz
+;	SOL	-	392Hz
+;	SOL#	-	415Hz
+;	LA	-	440Hz
+;	LA#	-	466Hz
+;	SI	-	494Hz
 
 	ORG	000H
 		
